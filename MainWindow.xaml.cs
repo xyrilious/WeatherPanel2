543576e21846e20f8696dc7505aaa40fd14e6bad
Xyrille Ann Mamalateo <xyrilleannm@gmail.com>
2019-09-12 19:16:24 +0800

    Weather Panel 2 Image Of Xyrille Mamalateo. #BeautyGF&BF

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XyrilleAnnMamalateoWeatherPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGetWeather_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new RestClient("https://api.darksky.net/forecast/64ee9d4e589bb2cb3788596fd477b0f7/14.8781,120.4546");
+            var request = new RestRequest("", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            var area = JsonConvert.DeserializeObject<WeatherArea>(content);
+
+
+            lblSummary.Content = area.Currently.Summary;
+            lblTemperature.Content = area.Currently.Temperature;
+            lblHumidity.Content = area.Currently.Humidity;
+            lblWindSpeed.Content = area.Currently.WindSpeed;
+            lblDateTime.Content = DateTime.Now.ToString("hh:mm tt");
+
+            BitmapImage bi = new BitmapImage();
+            // BitmapImage.UriSource must be in a BeginInit/EndInit block.
+            bi.BeginInit();
+            bi.UriSource = new Uri(@"/Images/xyrille.jpg" , UriKind.RelativeOrAbsolute);
+            bi.EndInit();
+
+            // Set the image source.
+            imageWeatherPanel.Source = bi;
+
+         
+
+            
+
+        }
+    }
+}
---
+++ WeatherArea.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XyrilleAnnMamalateoWeatherPanel
+{
+    public class WeatherArea
+    {
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+        public CurrentWeather Currently { get; set; }
+    }
+
+    public class CurrentWeather
+    {
+        public string Summary { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Temperature { get; set; }
+
+        public string Humidity { get; set; }
+
+        public string WindSpeed { get; set; }
+    }
+}
